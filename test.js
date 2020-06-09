7e10f0b309ac448ff66f5415aec0783b29a24410
London Bielicke <london.bielicke1313@gmail.com>
2020-06-09 09:35:00 -0500

    testing

---
+++ test.js
@@ -0,0 +1,29 @@
+var mind;
+var body;
+var soul;
+
+//basically data holder
+class Schedule {
+  constructor(work, hobbies) {
+    this.work = work;
+    this.hobbies = hobbies;
+  }
+}
+
+//current user schedule
+var mySchedule = new Schedule("kirkwood", "horses");
+//compute maximum mind body soul based on obligations
+
+//data for questions
+var questions = {
+  q1 = "a1";
+}
+
+
+
+//computes user's ideal schedule using mind body soul computations and preffered activities/obligations
+//while mind body and soul are not at maximum, try to find ideal schedule
+function scheduleComputer() {
+    var iHOS = 8;
+
+}
