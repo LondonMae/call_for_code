721fb47f9b22980e0990234d8297a5ab71e80270
London Bielicke <london.bielicke1313@gmail.com>
2020-06-13 13:33:18 -0500

    original javascript doc

--- test.js
+++ test.js
@@ -1,6 +1,10 @@
-var mind;
-var body;
-var soul;
+var mind = 100;
+var body = 100;
+var soul = 100;
+var idealFreeTime = 2.5;
+var idealSleep = 8;
+var idealExercise = 5;
+var totalExercise = 0;
 
 var currentSchedule = {
   one: false, two: false, three: false, four: false, five: false, six: false, seven: "wakeup", eight: "breakfast", nine: "run", ten: "yoga", eleven: "code", twelve: "lunch",
@@ -8,10 +12,10 @@ thirteen: "dogs", fourteen: "movies", fifteen: "movies", sixteen: "movies", seve
 twentytwo: "sleep", twentythree: false, twentyfour: false, oneNight: false, twoNight: false, threeNight: false, fourNight: false
 };
 
-var obligations = [ true, true, false, false, true, true, false, false, false, false, true, false, false, false, false, true ];
+var obligations = [ true, true, false, false, true, true, true, false, false, false, true, false, true, false, false, true ];
 //basically data holder
 class Schedule {
-  constructor(scheduleData, obligations, likesSleep, morningPerson, likesJob, missingSomething, coronaImpact, feelingOfSchedule, favoriteThings, dislikes) {
+  constructor(scheduleData, obligations, likesSleep, morningPerson, likesJob, mood, favoriteThings, dislikes, exercise, interaction) {
     this.wakeup = scheduleData[(indexOf["wakeup"] + 1)];
     this.bedtime = scheduleData[(indexOf["sleep"] + 1)];
     this.scheduleData = scheduleData;
@@ -19,20 +23,46 @@ class Schedule {
     this.sleepApproval = likesSleep;
     this.morningPerson = morningPerson;
     this.likesJob = likesJob;
-    this.missingSomething = missingSomething;
-    this.coronaImpact = coronaImpact;
-    this.feelingOfSchedule = feelingOfSchedule;
+    this.mood = mood;
+    this.favoriteThings = favoriteThings;
+    this.dislikes = dislikes;
+    this.exercise = exercise;
+    this.interaction = interaction;
+    this.freeTime = 0;
+    this.sleep = (24 - this.bedtime) + this.wakeup;
+  }
+
+  amountOfFreeTime() {
+    for (var i = 0; i <obligations.length; i++) {
+      if(!obligations[i]) { freeTime += 1; }
+    }
   }
 
   calcIdealness() {
-    //if answers are sad - points from all categories
-    //not enough sleep = subtract from mind and body
-    //too many obligations = subtract from call
-    //low engagment for hobbies = subtract from all
-    //no exercize = minus for body
-    //nothing educational = minus for mind
-    //no family or friends = minus for soul
+    var timeDiff = Math.abs(idealFreeTime-this.freeTime);
+    mind -= (timeDiff*10)/7;
+    body -= (timeDiff*5)/7;
+    soul -= (timeDiff*5)/7;
+
+    var sleepDiff = Math.abs(this.sleep - idealSleep);
+    mind -= (sleepDiff*7.5)/7;
+    body -= (sleepDiff*7.5)/7;
+
+    totalExercise += this.exercise;
+
+    if (!likesJob) {
+      mind -= 10;
+      soul -= 30;
+    }
+
+    if (mood > 40) {
+      soul -= mood/5;
+    }
+
   }
+
+
+
 
   get idealness(){
 
@@ -50,9 +80,6 @@ class Schedule {
 
   }
 
-  createSchedule() {
-
-  }
 
   get newSchedule(){
 
