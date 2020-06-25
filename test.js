326ea9f6884b36c923c8c3096bda940f16ea95c6
London Bielicke <london.bielicke1313@gmail.com>
2020-06-24 20:24:31 -0500

    guten tag

--- test.js
+++ test.js
@@ -15,24 +15,23 @@ var currentSchedule = [[ false, false, false, false, false, false, "wakeup", "br
 var obligations = [ true, true, false, false, true, true, true, false, false, false, true, false, true, false, false, true ];
 //basically data holder
 class Schedule {
-  constructor(scheduleData, likesSleep, morningPerson, likesJob, mood, favoriteThings, dislikes, exercise, interaction, age, work) {
+  constructor(scheduleData, likesJob, mood, favoriteThings, dislikes, exercise, interaction, age, work) {
     this.scheduleData = scheduleData;
     this.wakeup = this.wakeup();
     this.bedtime = this.bedtime();
-    this.sleepApproval = likesSleep;
-    this.morningPerson = morningPerson;
     this.age = 18;
-    this.likesJob = likesJob;
-    this.mood = mood;
+    this.likesJob = true;
+    this.mood = 30;
     this.favoriteThings = favoriteThings;
     this.dislikes = dislikes;
     this.exercise = this.exercise(); //turn into total
     this.idealExercise = this.idealExercise(); //calc ideal for age per week
-    this.interaction = interaction; //turn into daily array;
+    this.interaction = false; //turn into daily array;
     this.freeTime = this.amountOfFreeTime(); //turn into daily array
     this.sleep = this.sleep();
     this.idealSleep = this.idealSleep();
     this.idealFreeTime = this.idealFreeTime();
+    this.sleepDeviation = this.sleepDeviation();
     this.work = true;
   }
 
@@ -88,28 +87,50 @@ class Schedule {
 //change to work with new system
   amountOfFreeTime() {
 
-    var size = this.scheduleData.length;
-    this.scheduleData.forEach((item, i) => {
-      if (!item) { size--; }
-    });
+    var time = new Array();
+    for (var j = 0; j < 7; j++) {
+      var size = this.scheduleData.length;
+      this.scheduleData.forEach((item, i) => {
+        if (!item) { size--; }
+      });
 
-    var obligations = new Array(size);
+      var obligations = new Array(size);
 
-    var freeTime = 0;
-    for (var i = 0; i < obligations.length; i++) {
-      if(!obligations[i]) { freeTime += 1; }
+      var freeTime = 0;
+      for (var i = 0; i < obligations.length; i++) {
+        if(!obligations[i]) { freeTime += 1; }
+      }
+      time.push(freeTime);
     }
-    return freeTime;
+    console.log(time);
+    return time;
   }
 
   idealFreeTime() {
+    var free = new Array();
     for (var i = 0; i < 7; i++) {
-      if (this.work) { return 2.5; }
-      if (!this.work) { return 5; }
+      if (this.work) { free.push(2.5); }
+      if (!this.work) { free.push(5); }
     }
+    console.log(free);
+    return free;
   }
 
   sleepDeviation() {
+    var mean = 0;
+    this.bedtime.forEach((day, i) => {
+      mean += this.bedtime[i];
+    });
+    mean /= 7;
+
+    var sd = 0;
+    this.bedtime.forEach((day, i) => {
+      sd += Math.pow((this.bedtime[i] - mean), 2);
+
+    });
+    sd /= 7;
+    sd = Math.sqrt(sd);
+    return sd;
 
   }
 
@@ -137,7 +158,6 @@ class Schedule {
 
     //for loop for each day
     var timeDiff;
-    console.log(this.idealFreeTime);
     for (var i = 0; i < 7; i++) {
       timeDiff = Math.abs(this.idealFreeTime[i]-this.freeTime[i]);
       mind -= (timeDiff*10/7);
@@ -145,37 +165,43 @@ class Schedule {
       soul -= (timeDiff*5/7);
     }
 
+    var exerciseDiff = idealExercise - this.exercise;
+    if (exerciseDiff > 0) {body -= (exerciseDiff*15);}
 
-    // var exerciseDiff = idealExercise - this.exercise;
-    // if (exerciseDiff > 0) {body -= (exerciseDiff*15);}
-    //
-    // if (!this.likesJob) {
-    //   mind -= 10;
-    //   soul -= 30;
-    // }
-    //
-    // soul -= this.mood/5;
-    //
-    // if (!interaction) { soul -= 30; }
+    if (!this.likesJob) {
+      mind -= 10;
+      soul -= 30;
+    }
 
+    soul -= this.mood/5;
+
+    if (!this.interaction) { soul -= 30; }
+
+    this.mind -= this.sleepDeviation*6;
+    this.body -= this.sleepDeviation*4;
 
   }
 
-
+  //
 
 
   get idealness(){
 
   }
 
+  //keep all obligations
+  //eventually take into account future obligations
+  //add chosen mind related activities
   addMind() {
 
   }
 
+  //add chosen body related activities
   addBody(){
 
   }
 
+  //add chosen soul related activites
   addSoul(){
 
   }
@@ -185,12 +211,15 @@ class Schedule {
 
   }
 
+  //what's left?
+  // subtract the bad things, add the good things, account for social distancing
+
+
 }
 
 //current user schedule
 var mySchedule = new Schedule(currentSchedule, obligations, false, true, true, 30, "horses", "drinking", 1, true);
 mySchedule.calcIdealness();
-console.log(mySchedule.idealFreeTime);
 console.log(mind + " " + body + " " + soul);
 //compute maximum mind body soul based on obligations
 
